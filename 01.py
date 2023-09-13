550713aebdb075d3fe260c9b6e788791e8a15851
Pateluci <144231779+pateluci@users.noreply.github.com>
2023-09-13 12:04:43 +0000

    01

---
+++ 01.py
@@ -0,0 +1,11 @@
+vypis=print
+def vypis(x):
+    print(x)
+def soucet(a, b):
+    return a + b
+vysledek=soucet(19, 23)
+print(vysledek)
+vypis("cau lidi jak to jde")
+def sectiavypis(a, b):
+    print("soucet cisla", a,"a cisla", b,"je", a+ b)
+sectiavypis(1, 2)
