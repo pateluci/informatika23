533f73b18b628d7563b815a2f1a9393e930b4cb5
Pateluci <144231779+pateluci@users.noreply.github.com>
2023-09-26 21:50:40 +0200

    kvák

--- 02.py
+++ 02.py
@@ -1,1 +1,10 @@
+hadane_cislo = randint (1, 101)
+cislo = input
+
+if hadane_cislo == cislo:
+    print('Gratulujeme, uhádli jste číslo')
+elif hadane_cislo > cislo:
+    print(cislo, 'je větší než hádané číslo')
+elif hadane_cislo < cislo:
+    print(cislo,'je menší než hádané číslo')
 
