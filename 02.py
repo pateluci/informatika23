50e36c47d2d114d05dbe5cf1a48c430c22b707bb
Pateluci <144231779+pateluci@users.noreply.github.com>
2023-10-04 13:41:48 +0200

    bleh

--- 02.py
+++ 02.py
@@ -2,12 +2,14 @@ from random import randint
 cislo = int(input("hadejte cislo "))
 hadane_cislo = randint (1, 100)
 
+
 while hadane_cislo != cislo:
-    if hadane_cislo == cislo:
-        print('Gratulujeme, uhádli jste číslo')
-    elif hadane_cislo < cislo:
+    if hadane_cislo < cislo:
         print(cislo, 'je větší než hádané číslo')
         cislo = int(input("hadejte cislo "))
     elif hadane_cislo > cislo:
-        print(cislo,'je menší než hádané číslo')
+        print(cislo, 'je menší než hádané číslo')
         cislo = int(input("hadejte cislo "))
+    
+
+print(cislo, 'je hádané číslo, gratulujeme.')
