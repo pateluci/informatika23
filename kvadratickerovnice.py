0da19c784985bb2dc3cc2a050c240026d2dbaad6
Pateluci <144231779+pateluci@users.noreply.github.com>
2023-11-01 12:26:08 +0000

    bleh

--- kvadratickerovnice.py
+++ kvadratickerovnice.py
@@ -2,9 +2,14 @@ a=int(input("vložte a"))
 b=int(input("vložte b"))
 c=int(input("vložte c"))
 
-D=b**2 - 4*a*c
+def solve_quadratic_equasion(a, b, c):
+    D=b**2 - 4*a*c
 
-x1=(-b+D**0.5) /2*a
-x2=(-b-D**0.5) /2*a
+    x1=(-b+D**0.5) /2*a
+    x2=(-b-D**0.5) /2*a
+    print(x1, x2)   
 
-print(x1, x2)
+
+
+
+solve_quadratic_equasion(a, b, c)
