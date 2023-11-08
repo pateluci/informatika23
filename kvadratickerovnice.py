9174774ad7c79e8e330e02aeae13e0013e7764c1
Pateluci <144231779+pateluci@users.noreply.github.com>
2023-11-08 13:36:45 +0100

    Update kvadratickerovnice.py

--- kvadratickerovnice.py
+++ kvadratickerovnice.py
@@ -5,9 +5,18 @@ c=int(input("vložte c"))
 def solve_quadratic_equasion(a, b, c):
     D=b**2 - 4*a*c
 
-    x1=(-b+D**0.5) /2*a
+    if a==0 and b==0 and c==0:
+        print("Rovnice má nekonečně mnoho řešení")
+    elif a==0 and b==0:
+        print("Rovnice nemá řešení")
+    elif a==0:
+        print(-c/b)
+    else:
+        x1=(-b+D**0.5) /2*a
     x2=(-b-D**0.5) /2*a
     print(x1, x2)   
+
+
 
 
 
