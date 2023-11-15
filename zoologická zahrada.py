1a52553e765b8d450493bf908b43ece847e80ff5
Pateluci <144231779+pateluci@users.noreply.github.com>
2023-11-15 12:41:27 +0100

    Create zoologická zahrada.py

---
+++ zoologická zahrada.py
@@ -0,0 +1,1644 @@
+Adax
+
+Afrička ozdobná
+
+Agama dodomská
+
+Agama límcová
+
+Agama pobřežní
+
+Agama stepní
+
+Agama turkestánská
+
+Agama východoafrická
+
+Agama západoafrická
+
+Agamka písečná
+
+Achatina žravá
+
+Akuči zelený
+
+Alpaka
+
+Amazoňan jamajský
+
+Amazoňan modrobradý
+
+Amazoňan rudoocasý
+
+Amazoňan šedohlavý
+
+Amejva obecná
+
+Amuřice Hoevenova
+
+Anakonda velká
+
+Andulka vlnkovaná
+
+Anoa nížinný
+
+Anolis barakojský
+
+Anolis jeskynní
+
+Antilopa losí
+
+Antilopa vraná
+
+Ara hyacintový
+
+Ara Learův
+
+Aratinga andský
+
+Argus okatý
+
+Arowana dvojvousá
+
+Batagur bengálský
+
+Batagur kalagur
+
+Batagur tuntong
+
+Bazilišek zelený
+
+Bažant bronzový
+
+Bažant Edwardsův
+
+Bažant malajský
+
+Bažant palawanský
+
+Bažant paví
+
+Bažant Salvadoriho
+
+Bažant stříbrný
+
+Bažant tibetský
+
+Bažant turkménský
+
+Bažant vietnamský
+
+Bažant Wallichův
+
+Bažant zerafšanský
+
+Bažant zlatý
+
+Bělozubka nejmenší
+
+Bércoun africký
+
+Bércoun Petersův
+
+Bércoun rezavý
+
+Bércoun skvrnitý
+
+Berneška rudokrká
+
+Berneška velká labradorská
+
+Bičovka nosatá
+
+Bičovka stromová
+
+Binturong palawanský
+
+Bizon
+
+Blavor žlutý
+
+Bodlín Telfairův
+
+Bodlinatka egyptská
+
+Bodlinatka jihoafrická
+
+Bodlinatka turecká
+
+Bodlinatka zlatá
+
+Bojga ularburong
+
+Bongo horský
+
+Brhlík lesní
+
+Brkoslav severní
+
+Bukač velký
+
+Bukáček malý
+
+Bulbul černohlavý
+
+Bulbul červenouchý
+
+Bulbulčík bělohlavý
+
+Buvolec běločelý
+
+Chameleon obrovský
+
+Chápan středoamerický
+
+Charmozin Stellin (horský)
+
+Choholáček čelenkový
+
+Chřástal kropenatý
+
+Chřástal polní
+
+Chřestýš kostkovaný
+
+Chřestýšovec mangšanský
+
+Čája obojková
+
+Čáp bělokrký
+
+Čáp bílý
+
+Čáp černý
+
+Čáp simbil
+
+Čečetka tmavá
+
+Čečetka zimní
+
+Čejka australská
+
+Čejka běločelá
+
+Čejka chocholatá
+
+Čejka jihoamerická
+
+Čejka žlutonohá
+
+Čichavec drobnošupinný
+
+Čichavec líbající
+
+Čichavec zelený
+
+Čírka australasijská (bělohrdlá)
+
+Čírka hottentotská (diamantová)
+
+Čírka modrá
+
+Čírka obecná
+
+Čírka štíhlá
+
+Čírka úzkozobá
+
+Čírka žlutozobá
+
+Čížek lesní
+
+Člunozobec africký
+
+Čtverzubec zelený
+
+Čukvala zavalitá
+
+Daman pralesní
+
+Daman stepní
+
+Daman stromový
+
+Dánio bengálské
+
+Dánio dangila
+
+Dánio malabarské
+
+Dikdik Kirkův
+
+Dikobraz jihoafrický
+
+Dikobraz palawanský
+
+Dikobraz srstnatonosý
+
+Dlask tlustozobý
+
+Dlouhokrčka drsná
+
+Dlouhokrčka rotiská
+
+Dracéna krokodýlovitá
+
+Drozd černoprsý
+
+Drozd kvíčala
+
+Drozd oranžovohlavý
+
+Drozd tmavý
+
+Drozd zpěvný
+
+Drozdík tečkovaný
+
+Dudek chocholatý
+
+Dudkovec stromový
+
+Duhovka Bleherova
+
+Duhovka Boesemanova
+
+Duhovka diamantová
+
+Duhovka lososová
+
+Duhovka Wernerova
+
+Dvojzoborožec hnědavý
+
+Dvojzoborožec indický
+
+Dvojzoborožec nosorožčí
+
+Dytík bledý
+
+Dytík skvrnitý
+
+Dytík úhorní
+
+Dytík velký
+
+Ďábel medvědovitý
+
+Emu hnědý
+
+Felsuma Standingova
+
+Fenek
+
+Flétnák australský
+
+Frankolín žlutokrký
+
+Fryna malooká
+
+Gaviál indický
+
+Gazela dama tmavá
+
+Gekon
+
+Gekon létavý
+
+Gekon modrý
+
+Gekon obrovský
+
+Gekon skvrnitý
+
+Gekon turkestánský
+
+Gekon zázračný
+
+Gekončík východoafrický
+
+Gepard
+
+Gibon stříbrný
+
+Goral sečuánský
+
+Gorila nížinná
+
+Guan Grayův
+
+Gueréza pláštíková
+
+Gundi saharský
+
+Hadilov písař
+
+Havran polní
+
+Hlaváč sundský
+
+Hohol bělavý
+
+Hoko červenolaločnatý
+
+Hoko přilbový
+
+Holoubek skořicový
+
+Holub Bartlettův
+
+Holub bělolící filipínský
+
+Holub bronzovokřídlý
+
+Holub bronzový
+
+Holub černobronzový
+
+Holub domácí
+
+Holub doupňák
+
+Holub dvoubarvý
+
+Holub horský
+
+Holub chocholatý
+
+Holub kouřový
+
+Holub krvavý
+
+Holub modroocasý
+
+Holub nádherný
+
+Holub nikobarský
+
+Holub papouščí
+
+Holub pestrý
+
+Holub podkovní
+
+Holub pokřovní
+
+Holub příčkoprsý (H. kukaččí)
+
+Holub růžovotemenný
+
+Holub růžový
+
+Holub skalní
+
+Holub šedobřichý
+
+Holub wonga
+
+Holub zelenokřídlý
+
+Holub zlatoprsý
+
+Hrabáč kapský
+
+Hrabatka drsná
+
+Hrdlička čínská
+
+Hrdlička divoká
+
+Hrdlička chechtavá
+
+Hroch obojživelný
+
+Hrotočelec ohnivý
+
+Hrotočelec ozbrojený
+
+Hroznýš hnědý
+
+Hroznýš psohlavý
+
+Hroznýšek pestrý
+
+Hroznýšovec duhový
+
+Hroznýšovec kubánský
+
+Husa císařská
+
+Husa domácí
+
+Husa kuří
+
+Husa labutí domácí
+
+Husa malá
+
+Husice australská
+
+Husice rajská
+
+Husice rezavá
+
+Husička stromová
+
+Husovec stračí
+
+Hutie kubánská
+
+Hyena čabraková
+
+Hýl mexický
+
+Hýl obecný
+
+Ibis bělolící (americký)
+
+Ibis bílý
+
+Ibis černohlavý
+
+Ibis hagedaš
+
+Ibis hnědý
+
+Ibis madagaskarský
+
+Ibis modrooký
+
+Ibis rudý
+
+Ibis skalní
+
+Ibis slámokrký (žlutokrký)
+
+Ibis šedokřídlý
+
+Ilyodon Whiteův
+
+Irena tyrkysová
+
+Jaguarundi
+
+Jelen barmský
+
+Jelen bělohubý
+
+Jeřáb bělošíjí
+
+Jeřáb laločnatý
+
+Jeřáb mandžuský
+
+Jeřáb panenský
+
+Jeřáb popelavý
+
+Jeřáb rajský
+
+Jespák bojovný
+
+Ještěrka obecná
+
+Ještěrka paví
+
+Ještěrka zelená
+
+Ještěrkovec velký
+
+Jezevec šedý
+
+Ježek západní
+
+Ježura australská
+
+Kačka strakatá
+
+Kagu chocholatý
+
+Kachna bronzovokřídlá
+
+Kachna domácí
+
+Kachna patagonská
+
+Kachna temná
+
+Kachnice bělohlavá
+
+Kachnice kaštanová
+
+Kachnička amazonská
+
+Kachnička karolínská
+
+Kachnička mandarínská
+
+Kachnička obojková
+
+Kachnička šedoboká
+
+Kajmanka supí
+
+Kakadu bílý
+
+Kakadu havraní
+
+Kakadu palmový
+
+Kaloň egyptský
+
+Kaloň plavý
+
+Kaloň rodriguezský
+
+Kaložrout pruhovaný
+
+Kaložrout skvrnitý
+
+Kamiš růžkatý
+
+Kančil černavý
+
+Kandoja fidžijská
+
+Káně lesní
+
+Kapybara
+
+Karakal
+
+Kardinál dominikánský
+
+Karetka novoguinejská
+
+Kasuár přilbový
+
+Ketupa malajská
+
+Ketupa Pelova
+
+Kiang východní
+
+Kladivouš africký
+
+Klokan
+
+Klokan bažinný
+
+Klokan horský východní
+
+Klokan novoguinejský
+
+Klokan obrovský
+
+Klokan parma
+
+Klokan rudokrký tasmanský
+
+Klokan rudý
+
+Klokánek králíkovitý
+
+Klokánek krysí
+
+Kočka cejlonská
+
+Kočka krátkouchá
+
+Kočka palawanská
+
+Kočka rybářská
+
+Kočkodan Brazzův
+
+Kogna dlouhozobý
+
+Kogna jižní
+
+Kolpík africký
+
+Kolpík bílý
+
+Kolpík růžový
+
+Komba Garnettova
+
+Komba jižní
+
+Komba ušatá
+
+Kondor královský
+
+Kondor menší
+
+Konipas bílý
+
+Konopka obecná
+
+Korálovka Ruthvenova
+
+Korela chocholatá
+
+Kormorán černobílý
+
+Koroptev hnědoprsá (pruhokřídlá)
+
+Koroptev polní
+
+Korovec jedovatý
+
+Korunáč šedomodrý
+
+Kos černý
+
+Kotul veverovitý
+
+Koza domácí
+
+Kozorožec kavkazský
+
+Krajta Albertisova
+
+Krajta královská
+
+Krajta kýlnatá
+
+Krajta nachová
+
+Krajta tygrovitá
+
+Krajta zelená
+
+Králík domácí
+
+Kraska jávská
+
+Krátkokrčka červenobřichá
+
+Krátkokrčka novoguinejská
+
+Krkavec bělokrký
+
+Krkavec velký
+
+Krocan domácí
+
+Krokodýl kubánský
+
+Krokodýlovec čínský
+
+Kruhochvost nížinný
+
+Krysa akáciová
+
+Krysa obrovská
+
+Krysa velká
+
+Křeček velký
+
+Křepelka korunkatá
+
+Křepelka madagaskarská
+
+Křepelka polní
+
+Křovinář ostnitý
+
+Kubánka malá
+
+Kudlanka zelená
+
+Kukačka guira
+
+Kulíšek nejmenší
+
+Kůň Převalského
+
+Kur cejlonský
+
+Kur domácí
+
+Kuskus pozemní
+
+Kvakoš noční
+
+Kvakoš rezavý
+
+Kystráček indonéský
+
+Kystráček modrolící
+
+
+Labeo černé
+
+Labeo červenoocasé
+
+Labeo červenoploutvé
+
+Labeo krásnoploutvé
+
+Labuť Bewickova
+
+Lachtan jihoafrický
+
+Lama alpaka
+
+Lama guanako
+
+Lama vikuňa
+
+Latam vlaštovčí
+
+Ledňáček zelenohlavý
+
+Ledňák modrokřídlý
+
+Leguán chutný
+
+Leguán kubánský
+
+Leguán kyjoocasý
+
+Leguán modravý
+
+Leguán nosorohý
+
+Leguán pustinný
+
+Leguánek haitský
+
+Leguánek měnivý
+
+Leguánek modravý
+
+Leguánek ostnitý
+
+Leguánek pruhoocasý
+
+Leguánovec obojkový
+
+Leiocefalus haitský
+
+Lelkoun soví
+
+Lemčík černouchý
+
+Lemur běločelý
+
+Lemur černý
+
+Lemur kata
+
+Létavka kio
+
+Létavka obecná
+
+Létavka ušatá
+
+Lev indický
+
+Levhart jávský
+
+Levhart mandžuský
+
+Lezec
+
+Lezoun indický
+
+Linduška rudokrká
+
+Lori balijský
+
+Lori černohlavý
+
+Lori černý
+
+Lori horský
+
+Lori olivový
+
+Lori sumbawský
+
+Lori vlnkovaný
+
+Lori žlutoskvrnný
+
+Loríček červenohlavý
+
+Loríček oranžovoprsý
+
+Loríček rudobradý
+
+Loríček zlatouchý
+
+Lorikul modrobradý
+
+Los evropský
+
+Loskuták posvátný
+
+Luňák červený
+
+Luskoun krátkoocasý
+
+Lžičák pestrý
+
+Mabuja kapverdská
+
+Mabuja perleťová
+
+Mada modrotemenný
+
+Majna chocholatá
+
+Majna Rothschildova
+
+Majna žlutohlavá
+
+Majna žlutolící
+
+Makak magot
+
+Makak vepří
+
+Maki Ganzhornuv
+
+Maki Goodmanův
+
+Maloočka lovecká
+
+Mamba západoafrická
+
+Mandelík hajní
+
+Mangusta liščí
+
+Mangusta tmavá
+
+Mangusta trpasličí
+
+Mara slaništní
+
+Mara stepní
+
+Marabu africký
+
+Mečovka páskovaná
+
+Medojed
+
+Medvěd lední
+
+Mnohonožka červenonohá
+
+Mnohonožka krásná
+
+Mnohonožka obrovská
+
+Momot černolící
+
+Morčák bílý
+
+Morčák prostřední
+
+Morčák šupinatý
+
+Morče domácí
+
+Mravenečník velký
+
+Muntžak malý
+
+Myš čtyřpruhá
+
+Myš Mattheyova
+
+Myš nilská
+
+Myš páskovaná
+
+Myš pruhovaná
+
+Myš východoafrická
+
+Myš zebrovaná
+
+Myšák hnědokřídlý
+
+Myška africká
+
+Nahur modrý
+
+Nandu Darwinův
+
+Nefila zlatá
+
+Neoféma Bourkova (traváček Bourkův)
+
+Nestor kea
+
+Nesyt africký
+
+Nesyt bílý
+
+Noháč východoafrický
+
+Nosál bělohubý
+
+Nosál červený
+
+Nožovec Blankův
+
+Nožovec malý
+
+Nožovec pestrý
+
+Nožovec velký
+
+Nutrie říční
+
+Ocelot slaništní
+
+Ocelot stromový
+
+Okatec stříbřitý
+
+Orangutan sumaterský
+
+Orel bělohlavý
+
+Orel kejklíř
+
+Orel nejmenší
+
+Orel východní
+
+Orlicie bornejská
+
+Orlosup bradatý
+
+Osinák africký
+
+Ostnák jihoamerický
+
+Ostralka bělolící
+
+Ostralka štíhlá
+
+Ostříž lesní
+
+Outloň váhavý
+
+Ovce aljašská
+
+Ovce domácí
+
+Ovce tlustorohá
+
+Ovíječ filipínský
+
+Paještěrka očkatá
+
+Paještěrka písečná
+
+Paještěrka stepní
+
+Pakobylka okřídlená
+
+Panda červená
+
+Pangas vláknoploutvý
+
+Paovce hřivnatá
+
+Papoušek mniší
+
+Papoušek nádherný
+
+Papoušek patagonský chilský
+
+Papoušek vlnkovaný (= andulka vlnkovaná)
+
+Papoušek zpěvavý
+
+Papoušek žlutoramenný
+
+Pardál obláčkový
+
+Parmička černoskvrnná (vláknoploutvá)
+
+Parmička červenoocasá
+
+Parmička Dunckerova
+
+Parmička indická
+
+Parmička jamunská
+
+Parmička krásnoploutvá
+
+Parmička páskovaná
+
+Parmička pruhoocasá
+
+Parmička purpurová
+
+Parmička Schwanenfeldova
+
+Parmička velkoskvrnná
+
+Parmička žraločí
+
+Parmoun širokopruhý
+
+Pásovec kulovitý
+
+Páv korunkatý
+
+Pekari páskovaný
+
+Pekari Wagnerův
+
+Pelikán bílý
+
+Pelikán kadeřavý
+
+Pelikán skvrnozobý
+
+Pelomedusa africká
+
+Pěnkava jikavec
+
+Pěnkava obecná
+
+Perlička supí
+
+Pes pralesní
+
+Pes ušatý
+
+Pilohřbetec malošupinatý
+
+Pisila čáponohá
+
+Pískomil hedvábný
+
+Pískomil křečkovitý (kyjochvost)
+
+Pískomil štětkatý
+
+Pískomil veverkoocasý
+
+Pita kápovitá
+
+Pižmovka bělokřídlá
+
+Pižmovka hřebenatá
+
+Pižmovka jihoamerická
+
+Pižmovka konžská
+
+Pižmovka velká
+
+Pižmovka velká domácí
+
+Plameňák chilský
+
+Plameňák kubánský
+
+Plameňák růžový
+
+Plch savanový
+
+Polák černohlavý
+
+Polák hnědavý
+
+Polák malý
+
+Poštolka jižní
+
+Poštolka obecná
+
+Prase domácí
+
+Přímorožec beisa
+
+Přímorožec šavlorohý
+
+Přísavka thajská
+
+Psohlavec orinocký
+
+Psoun prériový
+
+Pštros dvouprstý
+
+Puštík bělavý
+
+Puštík bradatý
+
+Puštík brýlatý
+
+Rajka volavá
+
+Rájovec cejlonský
+
+Rákosnička ulugurská
+
+Razborka bornejská
+
+Rezatka čínská
+
+Ropucha coloradská
+
+Ropucha jihoafrická
+
+Ropucha obrovská
+
+Ropucha síťkovaná
+
+Ropušník velký
+
+Rosela černohlavá
+
+Rosela pestrá
+
+Rosnice siná
+
+Rosnice šalamounská
+
+Rosnička bělopruhá
+
+Rybák obecný
+
+Rypoš obří
+
+Rýžovník šedý
+
+Sambar ostrovní
+
+Saranče pustinná
+
+Saranče stěhovavá
+
+Satyr Cabotův
+
+Satyr Temminckův
+
+Scink ohnivý
+
+Scink šalomounský
+
+Scink válcovitý
+
+Sekavka nádherná
+
+Seriema rudozobá
+
+Sitatunga západoafrická
+
+Skalník sundský (modravec sundský)
+
+Sklípkan krátkonohý
+
+Sklípkan variabilní
+
+Skokan skřehotavý
+
+Skřivan ouškatý
+
+Skvrnivec proužkovaný
+
+Slavík Kaliopin (slavík kaliopa)
+
+Sleposcink Percivalův
+
+Slepýš křehký
+
+Slípka modrá
+
+Slon indický
+
+Slunatec nádherný
+
+Smrtonoš zmijí
+
+Snovač zahradní
+
+Sob karelský
+
+Sojkovec bělohrdlý
+
+Sojkovec černohrdlý
+
+Sojkovec dvoubarvý
+
+Sojkovec chocholatý
+
+Sojkovec jihočínský
+
+Sojkovec lesní
+
+Sojkovec modrotemenný
+
+Sojkovec nádherný
+
+Sojkovec popelavý
+
+Sojkovec rezavočelý
+
+Sojkovec rezavokřídlý
+
+Sojkovec rezavouchý
+
+Sojkovec stříbrouchý
+
+Sojkovec šedohnědý
+
+Sojkovec vlnkohřbetý
+
+Sojkovec zlatokřídlý (S. šupinkatý)
+
+Sova indická
+
+Sova pálená
+
+Sovice krahujová
+
+Sovice sněžní
+
+Stehlík obecný
+
+Stepokur hnědobřichý
+
+Stonoha
+
+Strnad obecný
+
+Stříkoun lapavý
+
+Sumeček průsvitný
+
+Sup hnědý
+
+Sup kapucín
+
+Sup mrchožravý
+
+Surikata
+
+Svišť lesní
+
+Sýc rousný
+
+Sýček obecný
+
+Sýkořice šedohlavá
+
+Sysel obecný
+
+Šafránka velká
+
+Šáma bělořitná
+
+Šípovec nosatý
+
+Šírohlavec východní
+
+Šnekojed kýlnatý
+
+Špaček čínský
+
+Špaček holohlavý
+
+Špaček obecný
+
+Špaček pagodový
+
+Špaček rudokřídlý
+
+Špaček rudooký
+
+Špaček růžový
+
+Štětkoun africký
+
+Štíhlovka Ravergierova (užovka Ravergierova)
+
+Štír nejdelší
+
+Štír ojedinělý (žlutopásý)
+
+Štíři
+
+Šváb Vanwaerebeckův
+
+Šváb americký
+
+Šváb egyptský
+
+Šváb harlekýn
+
+Šváb syčivý
+
+Šváb turkistánský
+
+Tabon lesní
+
+Tahr himálajský
+
+Takin indický
+
+Talapoin severní
+
+Tamarín bělovousý
+
+Tana severní
+
+Tapír čabrakový
+
+Tapír jihoamerický
+
+Tayra
+
+Teju červenavý
+
+Tenkozobec opačný
+
+Tetra rostlinožravá (metynis rostlinožravý)
+
+Timálie rudolící
+
+Timálie sečuánská
+
+Timálie zlatá
+
+Tinama přílbová
+
+Tinama tataupa
+
+Tkalčík bělohlavý
+
+Tkalčík rudozobý
+
+Tlamoun nilský
+
+Toko žlutozobý
+
+Traváček Bourkův
+
+Tricha orlí
+
+Trnoještěr Murphyho
+
+Trnorep mezopotamský
+
+Trnorep somálský
+
+Trnorep žíhaný
+
+Trnucha Castexova
+
+Trogon límcový
+
+Tučňák Humboldtův
+
+Turako bělobřichý
+
+Turako bělokorunkatý
+
+Turako šedý
+
+Tvrdoretka bantamská
+
+Tvrdoretka Hasseltova
+
+Tygr malajský
+
+Tygr sumaterský
+
+Tygr ussurijský
+
+Urzon kanadský
+
+Užovka amurská
+
+Užovka hladká
+
+Užovka nosatá
+
+Užovka obojková
+
+Užovka podplamatá
+
+Užovka stromová
+
+Užovka urartská
+
+Ústřičník velký
+
+Vakoveverka létavá
+
+Varan antracitový
+
+Varan komodský
+
+Varan molucký
+
+Varan plodožravý
+
+Varan smaragdový
+
+Varanovec bornejský
+
+Vari bělopásý
+
+Včelojed lesní
+
+Vejcožrout rezavý
+
+Velbloud dvouhrbý domácí
+
+Velemlok čínský
+
+Velemyš největší
+
+Velemyš obláčková
+
+Veleštír hojný
+
+Veleštír královský
+
+Veleštír promáčkloklepetatý
+
+Veverka bušová
+
+Veverka kapská
+
+Vikuňa
+
+Vlhovec červenohlavý
+
+Vlhovec Chapmanův
+
+Vlhovec chocholatý
+
+Vlhovec zelený
+
+Vlhovec žlutokápý
+
+Vlk eurasijský
+
+Vlk hřivnatý
+
+Vodnářka pruhovaná
+
+Vodnice posvátná
+
+Vodouš rudonohý
+
+Voduška abok
+
+Voduška červená
+
+Volavčík člunozobý
+
+Volavka červená manilská
+
+Volavka vlasatá
+
+Vombat obecný
+
+Vydra hladkosrstá
+
+Vydra severoamerická
+
+Výreček filipínský
+
+Wapiti manitobský
+
+Zákeřnice dvouskvrnná
+
+Zákeřnice hrozivá
+
+Zákeřnice mombo
+
+Zebra bezhřívá
+
+Zebra Böhmova
+
+Zebra Grévyho
+
+Zebřička australská
+
+Zebu zakrslý
+
+Zejozob africký
+
+Zlatohlávek
+
+Zlatohlávek bělolemý
+
+Zlatohlávek hnědožlutý
+
+Zlatohlávek konžský (Z. skvrnitý)
+
+Zlatohlávek smaragdový kamerunský
+
+Zlatohlávek somálský
+
+Zlatohlávek zelený
+
+Zmije nosorožčí
+
+Zmije obecná
+
+Zoborožec běloocasý
+
+Zoborožec bělovlasatý
+
+Zoborožec kaferský
+
+Zoborožec luzonský
+
+Zoborožec rýhozobý
+
+Zrzohlávka rudozobá
+
+Zubr
+
+Zvonek černohlavý
+
+Zvonek čínský
+
+Zvonek zelený
+
+Zvonohlík zahradní
+
+Želva amboinská
+
+Želva annamská
+
+Želva bahenní
+
+Želva černavá
+
+Želva dlaždicovitá
+
+Želva egyptská
+
+Želva hvězdnatá
+
+Želva korunkatá
+
+Želva kubánská
+
+Želva obrovská
+
+Želva ohebná
+
+Želva ostnitá
+
+Želva pardálí
+
+Želva pentličková
+
+Želva plochohřbetá
+
+Želva skalní
+
+Želva sloní pinzónská
+
+Želva sloní santacruzská
+
+Želva Smithova
+
+Želva Spenglerova
+
+Želva stepní
+
+Želva střechovitá
+
+Želva texaská
+
+Želva tlustohrdlá
+
+Želva tuniská
+
+Želva záhadná
+
+Želva zubatá
+
+Želva žlutočelá
+
+Žirafa severní núbijská (= Rothschildova)
+
+Žluna větší
+
+Žluva hajní
